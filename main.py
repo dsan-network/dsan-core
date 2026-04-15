f6b98a2f6f6f4130dc4603b4371313761f22fb47
SUBVERSO <submetaverso@gmail.com>
2026-04-15 02:25:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# main.py
 
+from agent.agent import DSANAgent
+from network.transport import send_message
+
+alice = DSANAgent("alice")
+
+message = "Hello DSAN"
+signature = alice.sign_message(message)
+
+payload = {
+    "sender": "alice",
+    "message": message,
+    "signature": signature.hex()
+}
+
+send_message("localhost", 5000, payload)
