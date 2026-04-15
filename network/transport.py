c276088bd9d5aacf64ebfa84f2210f0351095cc7
SUBVERSO <submetaverso@gmail.com>
2026-04-15 02:25:06 -0300

    Add send_message function to transport module

--- network/transport.py
+++ network/transport.py
@@ -1,1 +1,11 @@
+# network/transport.py
 
+import socket
+import json
+
+def send_message(host, port, payload):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+
+    client.send(json.dumps(payload).encode())
+    client.close()
